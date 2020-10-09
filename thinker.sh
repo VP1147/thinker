1eed96a06d7d823629cc8ab8de4c92d3629d4f43
Vinícius Pavão <pavao364@gmail.com>
2020-10-08 23:19:43 -0400

    Update thinker.sh

--- thinker.sh
+++ thinker.sh
@@ -4,8 +4,8 @@ SPEED=$(find /sys/devices/platform/ -name speed)
 SENS=$(find /sys/devices/platform/ -name sensitivity)
 
 if [ "$SPEED" ]; then
-    	echo "Found device. For granting access to this script, your password"
-    	echo "may be prompted."
+    	echo "$SPEED"
+    	echo "$SENS"
     	sudo chmod 666 $SPEED
     	sudo chmod 666 $SENS
 		python3 thinker.py $SPEED $SENS
