33248e03d4fa3939b049f237b12c1eec74515275
vp1147 <pavao364@gmail.com>
2020-10-09 13:13:16 -0400

    Added reset_button

--- thinker.py
+++ thinker.py
@@ -11,12 +11,18 @@ def fetch():
 		s2 = int(speed.read())
 	return s1, s2
 
-# If button clicked
-def on_button_clicked(button):
+# If set_button clicked
+def on_set(set_button):
 	with open(sys.argv[1], "w") as sens:
 		sens.write(str(int(sens_scale.get_value())))
 	with open(sys.argv[2], "w") as speed:
 		speed.write(str(int(speed_scale.get_value())))
+
+# If reset_button clicked
+def on_reset(reset_button):
+	s1, s2 = fetch()
+	sens_scale.set_value(s1)
+	speed_scale.set_value(s2)
 
 # Start builder
 builder = Gtk.Builder()
@@ -25,13 +31,19 @@ builder.add_from_file("layout.glade")
 # Import widgets from Glade
 win = builder.get_object("win")
 box = builder.get_object("box")
+
+# Scales
 speed_scale = builder.get_object("speed_scale")
 sens_scale = builder.get_object("accel_scale")
 s1, s2 = fetch()
 sens_scale.set_value(s1)
 speed_scale.set_value(s2)
-button = builder.get_object("button")
-button.connect("clicked", lambda x: on_button_clicked(button))
+
+# Buttons
+set_button = builder.get_object("set_button")
+set_button.connect("clicked", lambda x: on_set(set_button))
+reset_button = builder.get_object("reset_button")
+reset_button.connect("clicked", lambda x: on_reset(reset_button))
 
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
