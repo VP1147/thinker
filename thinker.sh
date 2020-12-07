7a5eaf2977fa258e591403f5af17e9f8be96146a
vp1147 <pavao364@gmail.com>
2020-12-07 10:27:26 -0400

    Update thinker.sh

--- thinker.sh
+++ thinker.sh
@@ -3,12 +3,22 @@
 SPEED=$(find /sys/devices/platform/ -name speed)
 SENS=$(find /sys/devices/platform/ -name sensitivity)
 
-if [ "$SPEED" ]; then
-    	echo "$SPEED"
-    	echo "$SENS"
-    	sudo chmod 666 $SPEED
-    	sudo chmod 666 $SENS
-		python3 thinker.py $SPEED $SENS
-else
-    echo "Couldn't find trackpoint device."
-fi
+#if [ "$SPEED" ]; then
+#    	echo "$SPEED"
+#    	echo "$SENS"
+#    	sudo chmod 666 $SPEED
+#    	sudo chmod 666 $SENS
+#	python3 thinker.py $SPEED $SENS
+#else
+#    echo "Couldn't find trackpoint device."
+#fi
+
+OUT=$(xinput list | grep "TrackPoint" | grep "pointer" | cut -d '=' -f 2 | cut -f 1)
+
+echo "Select the device to use: "
+for i in $OUT
+do
+	echo $(xinput list --name-only $i) "($i)"
+done
+
+python3 thinker.py $SPEED $SENS
